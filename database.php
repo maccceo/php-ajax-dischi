1054b5b6ead9e88846c169fb5a569880990bc7e6
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-09-10 12:32:02 +0200

    Init

--- database.php
+++ database.php
@@ -1,48 +1,5 @@
 <?php 
 
-    $albums = [
-        [
-         'name' => 'TuttoVenditti',
-         'artist' => 'Antonello Venditti',
-         'year' => '2012',
-         'genre' => 'piano rock',
-         'cover' => 'http://www.musictory.it/cover/2978/192487.jpg'
-        ],
-        [
-         'name' => 'TESTING',
-         'artist' => 'Asap Rocky',
-         'year' => '2018',
-         'genre' => 'rap',
-         'cover' => 'https://m.media-amazon.com/images/I/51O2XS+yQxL._AA256_.jpg'
-        ],
-        [
-         'name' => 'LONG.LIVE.A$AP',
-         'artist' => 'Asap Rocky',
-         'year' => '2014',
-         'genre' => 'rap',
-         'cover' => 'https://b3h2.scene7.com/is/image/BedBathandBeyond/103304847484993p?$imagePLP$&wid=256'
-        ],
-        [
-         'name' => 'ZUU',
-         'artist' => 'Denzel Curry',
-         'year' => '2019',
-         'genre' => 'rap',
-         'cover' => 'https://m.media-amazon.com/images/I/61ExaEJHUrL._AA256_.jpg'
-        ],
-        [
-         'name' => 'Big Fish Theory',
-         'artist' => 'Vince Staples',
-         'year' => '2018',
-         'genre' => 'country',
-         'cover' => 'https://m.media-amazon.com/images/I/61linIwAqbL._AA256_.jpg'
-        ],
-        [
-         'name' => "There's really a wolf",
-         'artist' => 'Russ',
-         'year' => '2016',
-         'genre' => 'pop',
-         'cover' => 'https://pm1.narvii.com/6853/7fe5b2af2fe7d92ce8bf9327cd15c4a06c5f4629v2_128.jpg'
-        ]
-    ];
+    $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
 
  ?>
--- index.php
+++ index.php
@@ -2,56 +2,27 @@
 <html lang="" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>MOOOSECA</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>Advanced Dashboard Charts</title>
+    <!-- FONT: LATO -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <!-- FONT: FONTAWESOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <!-- JS: JQUERY -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- JS: MOMENT -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+    <!-- CSS: MY STYLE -->
+    <link rel="stylesheet" href="style.css">
+    <!-- JS: MY SCRIPT -->
     <script type="text/javascript" src="script.js"></script>
-    <style>
-      body {font-family: Arial,sans-serif;}
-      
-      img {height: 150px;}
-
-      h1 {text-align: center; color: blue;}
-
-      .phpAlbum,.apiAlbum {
-        display: inline-block;
-        margin: 0 30px 20px;
-      }
-
-    </style>
+    <!-- TEMPLATE: MESSAGE MENU -->
+    <script id="item-template" type="text/x-handlebars-template">
+    </script>
   </head>
   <body>
   	<div id="main-container">
-      <h1>Aggiunti da PHP</h1>
-      <div id="php">
-        <?php 
-        //includo file db
-        include "database.php";
-        //stampo tutti gli album
-        foreach ($albums as $albumArray) {
-          echo '<div class="phpAlbum">' .
-            '<h2>' . $albumArray['name'] . "</h2>" .
-            '<h3>' . $albumArray['artist'] . "</h3>" .
-            '<p>' . $albumArray['year'] . ", " . $albumArray['genre'] . "</p>" . 
-            '<img src="' . $albumArray['cover'] . '">' .
-            '</div>';
-          }
-         ?>
-       </div>
-       <hr>
-       <h1>Aggiunti da API</h1>
-       <div id="api">
-         <select id="artist">
-           <option value="" disabled selected hidden>Filtra per artista</option>
-           <option value="Antonello Venditti">Antonello Venditti</option>
-           <option value="Asap Rocky">Asap Rocky</option>
-           <option value="Denzel Curry">Denzel Curry</option>
-           <option value="Vince Staples">Vince Staples</option>
-           <option value="Russ">Russ</option>
-           <option value="">- - Non filtrare - -</option>
-         </select>
-         <button id="go">Go</button>
-         <br>
-       </div>
     </div>
   </body>
 </html>
